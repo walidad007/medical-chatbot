a99c3d20ffe7f0d1e6e478a086d66b81a42dc723
walidad007 <walidadmahtam62@gmail.com>
2025-01-13 20:09:22 +0500

    untracked env

--- create_memory_for_llm.py
+++ create_memory_for_llm.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 
-# step 1 : Load raw PDF(s)
+# Step 1 : Load raw PDF(s)
 DATA_PATH = "data/"
 def load_pdf_files(data):
     loader = DirectoryLoader(data,
@@ -20,7 +20,7 @@ documents = load_pdf_files(data=DATA_PATH)
 # print("Lenght of PDF Pages: ", len(documents))
 
 
-# step 2 : Create Chunks 
+# Step 2 : Create Chunks 
 def create_chunks(extracted_data):
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=500,
                                                     chunk_overlap=50)
@@ -30,7 +30,7 @@ def create_chunks(extracted_data):
 text_chunks = create_chunks(extracted_data=documents)
 # print("Lenght of Text Chunks: ", len(text_chunks))
 
-# step 3 : Create Vector Embeddings
+# Step 3 : Create Vector Embeddings
 def get_embedding_model():
     embedding_model = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return embedding_model
@@ -38,7 +38,7 @@ def get_embedding_model():
 embedding_model = get_embedding_model()
 
 
-# step 4 : Store embeddings in FAISS
+# Step 4 : Store embeddings in FAISS
 DB_FAISS_PATH = "vectorstore/db_faiss"
 db = FAISS.from_documents(text_chunks, embedding_model)
 db.save_local(DB_FAISS_PATH)
